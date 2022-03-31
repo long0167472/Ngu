ac3e9132377237c43a34fea61a98a9930bb78892
long0167472 <Hoanglong7374@gmail.com>
2022-04-01 02:52:39 +0700

    ...

---
+++ admin-login/admin-login.js
@@ -0,0 +1,44 @@
+//Function for checking email
+function checkEmailValidate(email) {
+    let emailPattern = /^[0-9]{3}(adminFB)(@autohub.cc)$/;
+    // The email starts with 3 number
+    //after the number must be the "admin"
+    // The email ends with @autohub.cc
+
+
+
+    if (emailPattern.test(email == false)) {
+        document.getElementById("emailHelp1").textContent = 'Invalid';
+        document.getElementById("emailHelp1").style.color = 'red';
+        check1 = false;
+    }
+
+}
+//function for checking password
+function checkPasswordValidate(pass) {
+    let passPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    // password has at least (1 upper case, 1 lower case, 1 symbol, 1 number,)
+    //the password must contain at least 8 chars 
+    if (passPattern.test(pass == false)) {
+        document.getElementById("passHelp1").textContent = 'Password incorect!';
+        document.getElementById("passHelp1").style.color = 'red';
+        check2 = false;
+    }
+}
+// All the element joins the logining process
+let txtEmail = document.getElementById("txtEmail");
+let txtPass = document.getElementById("txtPass");
+let btnLogin = document.getElementById("btnforlogin");
+let check1 = false; //the check1 is to see whether the email is correct with standard of Regex or not
+let check2 = false; //the check2 is to see whether the password is correct with standard of Regex or not
+let loginA = document.getElementById("loginA");
+
+// Function
+btnLogin.addEventListener("click", Func1);
+
+function Func1() {
+    checkEmailValidate(txtEmail.value);
+    checkPasswordValidate(txtPass.value);
+    if (check1 == true & check2 == true)
+        loginA.href = "#";
+}
